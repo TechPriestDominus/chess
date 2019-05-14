13c6571a43bebbc95113318589412215e71ef636
TechPriestDominus <smolodtsov58@gmail.com>
2019-05-14 09:02:02 +0700

    Eddit version

--- src/chess_print_plain.c
+++ src/chess_print_plain.c
@@ -3,20 +3,23 @@
 extern char board[8][8];
 
 void chesspp (char *board) {
-    int i,j;
-    for(i=0;i<8;i++) {
-        printf("%d ", 8-i);
-        for(j=0;j<8;j++) {
-            printf("%c ", board[i][j]);
-        }
-        printf("\n");
-    }
-    printf("  ");
-    for (i = 0; i < 8; i++) {
-        printf("%c ", i + 97);
-    }
-printf("\n");
+	int i, j;
+	printf("\n");
 
+	for (i = 7; i >= 0; i--) {
+		printf("                                ");
+		printf("%d ", i + 1);
+		for (j = 0; j < 8; j++) {
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("                                  ");
+	for (i = 0; i < 8; i++) {
+		printf("%c ", i + 97);
+	}
+	printf("\n");
 }
 
 
